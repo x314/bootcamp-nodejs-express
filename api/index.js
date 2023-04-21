754eb55b18f9cb085cc47fd65d421d99050f6c49
Alex Villanueva Mercado <alvimer2708@gmail.com>
2023-04-21 17:50:14 -0500

    deployment

--- api/index.js
+++ api/index.js
@@ -21,11 +21,11 @@ const options = {
 }
 app.use(cors(options))
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
   res.send("Hola my server express")
 })
 
-app.get("/nueva-ruta", (req, res) => {
+app.get("/api/nueva-ruta", (req, res) => {
   res.send("Hola, soy una nueva ruta")
 })
 
